a7db7c594b98df68a2f1a444ea5982a9ad5bb89c
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-06-28 18:51:14 +0530

    Fix app/build.gradle.kts - remove jcenter, use mavenCentral only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,6 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-    jcenter()
-    maven { url = uri("https://jitpack.io") }
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
